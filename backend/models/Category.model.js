fa714cf3cca11af608672224192c473862a2f5f1
guillainbisimwa <guillainbisimwa@hotmail.fr>
2022-03-03 21:47:43 +0200

    Add category model

---
+++ backend/models/Category.model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const Category = mongoose.Schema({
+    label : String,
+    details : String,
+    status : Number, default: 0,
+    value: Number,
+    created: {type: Date, default: Date.now}
+});
+
+module.exports = mongoose.model('categories', Category);
