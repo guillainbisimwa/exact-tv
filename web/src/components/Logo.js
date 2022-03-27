50d7701e3df957269e1ff0b1ada9f8e444329ef1
guillainbisimwa <guillainbisimwa@hotmail.fr>
2022-03-27 22:07:21 +0200

    Add components

---
+++ web/src/components/Logo.js
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+import { Link as RouterLink } from 'react-router-dom';
+// material
+import { Box } from '@mui/material';
+import logo from './../assets/logo/logow.png';
+
+
+// ----------------------------------------------------------------------
+
+Logo.propTypes = {
+  sx: PropTypes.object
+};
+
+export default function Logo({ sx }) {
+  return (
+    <RouterLink to="/">
+      <Box component="img" src={logo} sx={{ width: 40, height: 40, ...sx }} />
+    </RouterLink>
+  );
+}
