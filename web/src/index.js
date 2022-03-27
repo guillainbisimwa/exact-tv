bee235531d3e2d88e149a9930bd1aac85bac3a1f
guillainbisimwa <guillainbisimwa@hotmail.fr>
2022-03-27 20:57:06 +0200

    Add main Index file:

--- web/src/index.js
+++ web/src/index.js
@@ -1,7 +1,13 @@
 import App from "./App";
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.render(
- <App />,
+  <HelmetProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </HelmetProvider>,
   document.getElementById('root')
 );
