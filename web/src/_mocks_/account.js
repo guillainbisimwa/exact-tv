6d968b16f7287ec010bccb3f34659719773b1c15
guillainbisimwa <guillainbisimwa@hotmail.fr>
2022-03-28 16:43:46 +0200

    Add Account mock

---
+++ web/src/_mocks_/account.js
@@ -0,0 +1,9 @@
+// ----------------------------------------------------------------------
+
+const account = {
+  displayName: 'Nom Postnom',
+  email: 'demo@exact.tv',
+  photoURL: './../assets/avatar.png'
+};
+
+export default account;
