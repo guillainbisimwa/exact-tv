c583bc8748fcdd9a818f1e04d69344a78090c228
guillainbisimwa <guillainbisimwa@hotmail.fr>
2022-03-31 23:51:31 +0200

    Add Rating stars

--- web/src/sections/@Home/Banner/Banner.js
+++ web/src/sections/@Home/Banner/Banner.js
@@ -1,8 +1,18 @@
-import { Grid, Card, Typography } from '@mui/material';
+import { Grid, Box, Typography } from '@mui/material';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
-import { productionHouse } from '../../../_mocks_';
+import { styled } from '@mui/material/styles';
 
+import { productionHouse } from '../../../_mocks_';
+import Iconify from '../../../components/Iconify';
+
+const InfoStyle = styled('div')(({ theme }) => ({
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'flex-start',
+    marginTop: theme.spacing(0),
+    color: theme.palette.text.disabled
+  }));
 
 const Banner = ({film, index }) => {
     const {
@@ -39,6 +49,33 @@ const Banner = ({film, index }) => {
         <Typography variant='h6' color="#aaa" >Nouveau</Typography>
         <Typography variant='h3' fontWeight="bold" color="#fff" >{title}</Typography>
         <Typography variant='h6' color="#aaa" >{studio.name} Studio</Typography>
+
+        <InfoStyle>
+            <Typography  color="#aaa" mr={2}> Rating </Typography>
+            {[...Array(5-stars)].map((index) => (
+              <Box
+                key={index}
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                }}
+              >
+                <Iconify icon='eva:star-fill' sx={{ width: 16, height: 16, mr: 0.3, color: "#f5dd0b" }} />
+              </Box>
+            ))}
+
+            {[...Array(5 -(5-stars))].map((index) => (
+              <Box
+                key={index}
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                }}
+              >
+                <Iconify icon='eva:star-fill' sx={{ width: 16, height: 16, mr: 0.3, }} />
+              </Box>
+            ))}
+          </InfoStyle>
 
     </Grid>
   );
